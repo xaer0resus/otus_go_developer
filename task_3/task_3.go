e683f2504ce7caca403ce45a9026ee40040809a5
xaer0resus <145779989+xaer0resus@users.noreply.github.com>
2025-10-22 19:33:31 +0300

    task 3

--- task_3/task_3.go
+++ task_3/task_3.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func createChessBoard(size int) string {
+	const (
+		blackTile = " "
+		whiteTile = "#"
+	)
+
+	var boardBuilder strings.Builder
+	for i := 0; i < size; i++ {
+		var pattern string
+		if i%2 == 0 {
+			pattern = blackTile + whiteTile
+		} else {
+			pattern = whiteTile + blackTile
+		}
+		boardBuilder.WriteString(strings.Repeat(pattern, size/2) + "\n")
+	}
+	return boardBuilder.String()
+}
 
 func main() {
-	fmt.Println("Hello Otus Go Developer")
+	var size int
+	fmt.Print("Введите желаемый размер шахматной доски: ")
+	_, err := fmt.Scan(&size)
+	if err != nil {
+		fmt.Println("Ошибка ввода: введите целое число")
+		return
+	}
+	if size <= 0 {
+		fmt.Println("Ошибка ввода: введите положительное число")
+	}
+	fmt.Print(createChessBoard(size))
 }
